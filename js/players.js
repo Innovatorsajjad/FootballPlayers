520f501f9dc6d7ac8c19e989cd1040ff358d5861
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-03-02 04:30:15 +0600

    spinner done

--- js/players.js
+++ js/players.js
@@ -1,14 +1,15 @@
  //Detect search Button.........
  const SearchButton = () =>{
+     document.getElementById("spinner").style.display="block";
+     document.getElementById("player_container").innerHTML='';
     const searchValue = document.getElementById("Search_Box").value;
-    searchValue.value=""
     const url= `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`
     fetch(url)
     .then(Response=>Response.json())
     .then(data => DisplayPlayerDetails (data.player));
-    
-     
-}
+         
+    }
+
 
 const DisplayPlayerDetails = (Players) =>{
  for(const player of Players){
@@ -16,7 +17,7 @@ const DisplayPlayerDetails = (Players) =>{
     const div = document.createElement("div");
     div.classList.add("col-lg-4");
     div.classList.add("mb-3");
-
+    document.getElementById("spinner").style.display="none"
     div.innerHTML=`
     <div class="card mx-auto text-center" style="width: 18rem;">
       <img src="${player.strThumb}" class="card-img-top" alt="...">
@@ -44,10 +45,12 @@ const Details = (info)=> {
 const LoadData = (info) => {
  console.log(info);
  const cardInfo = document.getElementById("Details_container").innerHTML=`
- <div class="card w-75 ">
+ <div class="card w-75 text-center">
   <div class="card-body">
-    <h5 class="card-title">${info.strPlayer}</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+  <img src="${info.strThumb}" class="card-img-top rounded-circle w-75" alt="...">
+  <h3 class="card-title">Nationality: ${info.strNationality}</h3>
+    <h5 class="card-title">Name: ${info.strPlayer}</h5>
+    <p class="card-text"> Description: ${info.strDescriptionEN.slice(0,400)}</p></p>
     
   </div>
 </div>
