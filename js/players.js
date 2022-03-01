8453e393c44a5ff067b0ffb699b821ae53bd4c21
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-03-02 02:01:29 +0600

    dynamic name added

--- js/players.js
+++ js/players.js
@@ -2,11 +2,12 @@
  const SearchButton = () =>{
 console.log('clicked');
     const searchValue = document.getElementById("Search_Box").value;
-
+    searchValue.value=""
     const url= `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`
     fetch(url)
     .then(Response=>Response.json())
     .then(data => DisplayPlayerDetails (data.player));
+    
      
 }
 
@@ -15,16 +16,17 @@ const DisplayPlayerDetails = (Players) =>{
     const parentDiv= document.getElementById("player_container");
     const div = document.createElement("div");
     div.classList.add("col-lg-4");
+    div.classList.add("mb-3");
 
     div.innerHTML=`
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
+    <div class="card mx-auto text-center" style="width: 18rem;">
+      <img src="${player.strThumb}" class="card-img-top" alt="...">
        <div class="card-body">
-           <h5 class="card-title">Card title</h5>
-           <p class="card-text">text</p>
+           <h5 class="card-title">${player.strPlayer}</h5>
+           <p class="card-text">${player.strNationality}</p>
            <div class="buttons d-flex justify-content-around">
            <a href="#" class="btn btn-danger">Delete</a> 
-           <a href="#" class="btn btn-success">Details</a>
+           <a href="#" class="btn btn-success onclick()="Details">Details</a>
       </div>
     </div>
     </div>
