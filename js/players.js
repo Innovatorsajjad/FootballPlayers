954fe8cd4c811f78b60979827d0d1bb64204e202
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-03-02 01:34:03 +0600

    card added

--- js/players.js
+++ js/players.js
@@ -1,12 +1,36 @@
  //Detect search Button.........
  const SearchButton = () =>{
+console.log('clicked');
     const searchValue = document.getElementById("Search_Box").value;
 
     const url= `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`
     fetch(url)
     .then(Response=>Response.json())
-    .then(data => console.log(data))
-
-
-
+    .then(data => DisplayPlayerDetails (data.player));
+     
 }
+
+const DisplayPlayerDetails = (Players) =>{
+ for(const player of Players){
+    const parentDiv= document.getElementById("player_container");
+    const div = document.createElement("div");
+    div.classList.add("col-lg-4");
+
+    div.innerHTML=`
+    <div class="card" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="...">
+       <div class="card-body">
+           <h5 class="card-title">Card title</h5>
+           <p class="card-text">text</p>
+           <div class="buttons d-flex justify-content-around">
+           <a href="#" class="btn btn-danger">Delete</a> 
+           <a href="#" class="btn btn-success">Details</a>
+      </div>
+    </div>
+    </div>
+    `
+    parentDiv.appendChild(div);
+ };
+   };
+
+
